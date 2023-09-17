a92ebe56a3b8b3d4eedaee5904931ee7e0c902b6
unknown <mithuweb000@gmail.com>
2023-09-17 08:50:44 +0600

    init setup

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+function App() {
+  return <h2>Context API</h2>;
+}
+export default App;
---
+++ src/cartItems.js
@@ -0,0 +1,36 @@
+const cartItems = [
+  {
+    id: 'rec1JZlfCIBOPdcT2',
+    title: 'Samsung Galaxy S8',
+    price: '399.99',
+    img:
+      'https://images.pexels.com/photos/2066896/pexels-photo-2066896.jpeg?auto=compress&cs=tinysrgb&w=600',
+    amount: 1,
+  },
+  {
+    id: 'recB6qcHPxb62YJ75',
+    title: 'google pixel',
+    price: '499.99',
+    img:
+      'https://images.pexels.com/photos/719399/pexels-photo-719399.jpeg?auto=compress&cs=tinysrgb&w=600',
+    amount: 1,
+  },
+  {
+    id: 'recdRxBsE14Rr2VuJ',
+    title: 'Xiaomi Redmi Note 2',
+    price: '699.99',
+    img:
+      'https://images.pexels.com/photos/3999536/pexels-photo-3999536.jpeg?auto=compress&cs=tinysrgb&w=600',
+    amount: 1,
+  },
+  {
+    id: 'recwTo160XST3PIoW',
+    title: 'Samsung Galaxy S7',
+    price: '599.99 ',
+    img:
+      'https://images.pexels.com/photos/1394641/pexels-photo-1394641.jpeg?auto=compress&cs=tinysrgb&w=600',
+    amount: 1,
+  },
+];
+
+export default cartItems;
